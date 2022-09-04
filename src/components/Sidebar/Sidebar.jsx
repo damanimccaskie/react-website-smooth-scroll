9d9af7e62fb063564129cd005ce941619c9dad8b
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-09-04 10:30:15 -0400

    Initialize GIT and Finish Navbar and Start Sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,33 @@
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from "./Sidebar.styles";
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
